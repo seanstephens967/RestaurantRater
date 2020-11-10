396dadf79e04f3167440f05f48f24fbc7d0ef035
seanstephens967 <seanstephens967@gmail.com>
2020-11-10 11:47:40 -0500

    section 2

---
+++ RestaurantRater/Models/Restaurant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantRater.Models
+{
+    public class Restaurant
+    {
+        public int RestaurantID { get; set; }
+        public int Name { get; set; }
+        public int Address { get; set; }
+        public int Rating { get; set; }
+    }
+}
